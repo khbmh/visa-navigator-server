9a6db48649210c90573b2f59fe1c88172f55d2a9
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2024-12-08 11:26:11 +0600

    base ready

---
+++ index.js
@@ -0,0 +1,20 @@
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
